bda5cd8ed1598f850ab383112f45373fe8ab7ae4
goripo <24ynaka@gmail.com>
2024-09-05 14:08:40 +0900

    all done

---
+++ pages/index.js
@@ -0,0 +1,65 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import Layout, { siteTitle } from "@/components/Layout";
+
+import Link from "next/link";
+import utilStyle from "../styles/utils.module.css";
+import { getPostsData } from "../lib/post";
+
+//SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData(); //id, title, date, thumbnail
+  console.log(allPostsData);
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// //SSRの場合
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       //コンポーネントに渡すためのprops
+//     },
+//   };
+// }
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyle.headingMd}>
+        <p>
+          私の名前はゴリぽんです/UOH/ゴリぽんとは
+        </p>
+      </section>
+
+      <section>
+        <h2>📝ブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({id, title, date, thumbnail}) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img src={`${thumbnail}`} className={styles.thumbnailImage}/>
+              </Link>
+              <Link href={`/posts/${id}`} legacyBehavior>
+                <a className={utilStyle.boldText}>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>{date}</small>
+            </article>
+          ))}
+          
+        </div>
+      </section>
+
+      
+    </Layout>
+  );
+}
